a26e184fbddd885508172aeee271a664ee4b9ca9
Arthur Garcia Torrezani <79718431+ArthurGarciaTorrezani@users.noreply.github.com>
2024-07-04 08:21:45 -0300

    Update ListaSE.java

--- ListaSE.java
+++ ListaSE.java
@@ -1,20 +1,32 @@
 public class ListaSE {
-     Celula primeiro;
-     public ListaSE(){
-          primeiro = null;          
-     }
+    Celula primeiro;
+    public ListaSE(){
+        primeiro = null;
+    }
 
-     public void inserirNoInicio(Celula c){
-          c.prox = primeiro;
-          primeiro = c;
-     }
+    public void inserirNoInicio(Celula c){
+        c.prox = primeiro;
+        primeiro = c;
+    }
 
-     public void imprimir(){
-          Celula aux = primeiro;
-          while (aux.prox != null) {
-               System.out.println("valor:"+aux.valor);
-               aux = aux.prox;
-          }
-          System.out.println("valor:"+aux.valor);
-     }
+    public void inserirNoIFinal(Celula c){
+        if(c == null){
+
+        }else{
+            Celula aux = primeiro;
+            while (aux.prox != null){ // 1.2    2.3   3.0
+                aux = aux.prox;       // 1.2
+            }
+            aux.prox = c;
+        }
+    }
+
+    public void imprimir(){ //1.2 2.3 3.0
+        Celula aux = primeiro;
+        while (aux != null) {
+            System.out.println("valor:"+aux.valor);
+            aux = aux.prox;
+        }
+
+    }
 }
